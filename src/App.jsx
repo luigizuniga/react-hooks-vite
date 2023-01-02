2994fd8429d2666aafbaa23a31e32cea1341eb4b
luigizuniga <luigialbertozuniga@gmail.com>
2023-01-02 01:42:15 -0300

    useEffect example with custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
+// UseState Components
 import { HooksApp } from './components/useState/HooksApp'
 import { CounterApp } from './components/useState/CounterApp'
 import { CounterWithCustomHook } from './components/useState/CounterWithCustomHook'
 
+
+// useEffect Components
+import { SimpleForm } from './components/useEffect/SimpleForm'
+import { FormWithCustomHooks } from './components/useEffect/FormWithCustomHooks'
+
+// Styles
 import './App.css'
 
 function App() {
   return (
     <>
-    <div className="App">
-       <HooksApp/>
-       <CounterApp />
-       <CounterWithCustomHook />
-    </div>
+      <div className="App">
+        {/* <HooksApp/> */}
+        {/*<CounterApp />*/}
+        {/*<CounterWithCustomHook /> */}
+
+        {/* <SimpleForm /> */}
+        <FormWithCustomHooks />
+      </div>
     </>
   )
 }
